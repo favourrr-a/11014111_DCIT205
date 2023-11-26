944841f8258ba01576d6eb9b8acb0727f5c29d4b
nerdfavour <favourackonu13@gmail.com>
2023-11-26 21:56:41 +0000

    did academics and undergraduate page

---
+++ undergrad.js
@@ -0,0 +1,21 @@
+var levelButtonsCS = document.getElementsByClassName('buttonForCSCourses');
+var levelButtonsIT = document.getElementsByClassName('buttonForITCourses')
+for (var i = 0; i < levelButtonsCS.length; i++) {
+    levelButtonsCS[i].addEventListener('click', function() {
+        var level = this.getAttribute('data-level');
+        var levelContent = document.getElementById('cs' + level);
+        
+        // Toggle the display property
+        levelContent.style.display = (levelContent.style.display === 'none' || levelContent.style.display === '') ? 'block' : 'none';
+    });
+}
+
+for (var i = 0; i < levelButtonsIT.length; i++) {
+    levelButtonsIT[i].addEventListener('click', function() {
+        var level = this.getAttribute('data-level');
+        var levelContent = document.getElementById('it' + level);
+        
+        // Toggle the display property
+        levelContent.style.display = (levelContent.style.display === 'none' || levelContent.style.display === '') ? 'block' : 'none';
+    });
+}
