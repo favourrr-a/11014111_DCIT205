c435fa810cbde80c4c8cc116cfbf4b854d1a01ab
nerdfavour <favourackonu13@gmail.com>
2023-11-27 15:31:40 +0000

    done w phd page

---
+++ phd.js
@@ -0,0 +1,20 @@
+document.getElementById('buttonForY1').addEventListener('click', function () {
+    var hiddenContent = document.getElementById('phdY1');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
+
+document.getElementById('buttonForY2').addEventListener('click', function () {
+    var hiddenContent = document.getElementById('phdY2');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
+
+document.getElementById('buttonForY3').addEventListener('click', function () {
+    var hiddenContent = document.getElementById('phdY3');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
+
+document.getElementById('buttonForY4').addEventListener('click', function () {
+    var hiddenContent = document.getElementById('phdY4');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
+
