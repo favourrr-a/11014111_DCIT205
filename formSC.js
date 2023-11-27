86d87345a53bde4782db25210c4a618cb39b18ca
nerdfavour <favourackonu13@gmail.com>
2023-11-27 17:07:47 +0000

    done with shortcourses page

---
+++ formSC.js
@@ -0,0 +1,4 @@
+document.getElementById('registerSCB').addEventListener('click', function() {
+        var googleFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSdR3QRd-knezcQE0W8X-V2c_J_MeuZbfMxkNTLy6jwkqUKFPA/viewform'; 
+    window.location.href = googleFormUrl;
+});
