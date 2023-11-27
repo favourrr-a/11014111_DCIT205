a9b9cea946134d6862edead3c046e72d84db96ac
nerdfavour <favourackonu13@gmail.com>
2023-11-27 00:58:49 +0000

    added 'get to know' to the left of  the pages under academics

---
+++ academics.js
@@ -0,0 +1,20 @@
+
+document.getElementById('navClickToViewMore2').addEventListener('click', function () {
+    var hiddenContent = document.getElementById('hiddenItemNavClickToViewMore2');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
+
+document.getElementById('navClickToViewMore1').addEventListener('click', function () {
+    var hiddenContent = document.getElementById('hiddenItemNavClickToViewMore1');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
+
+document.getElementById('navClickToViewMore3').addEventListener('click', function () {
+    var hiddenContent = document.getElementById('hiddenItemNavClickToViewMore3');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
+
+document.getElementById('navClickToViewMore4').addEventListener('click', function() {
+    var hiddenContent = document.getElementById('hiddenItemNavClickToViewMore4');
+    hiddenContent.style.display = (hiddenContent.style.display === 'none' || hiddenContent.style.display === '') ? 'block' : 'none';
+});
