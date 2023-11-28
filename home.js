68da3a54f87a00a8a94b6c55c57ff74d49e57fec
nerdfavour <favourackonu13@gmail.com>
2023-11-28 05:09:35 +0000

    changed font and colors of website

---
+++ home.js
@@ -0,0 +1,32 @@
+document.getElementById('openPageButton1').addEventListener('click', function() {
+    
+    var targetPageUrl = 'undergrad.html'; // Replace with your target page URL
+
+    
+    window.location.href = targetPageUrl;
+});
+
+document.getElementById('openPageButton2').addEventListener('click', function() {
+    
+    var targetPageUrl = 'mscmphil.html'; // Replace with your target page URL
+
+    
+    window.location.href = targetPageUrl;
+});
+
+document.getElementById('openPageButton3').addEventListener('click', function() {
+    
+    var targetPageUrl = 'phd.html'; // Replace with your target page URL
+
+    
+    window.location.href = targetPageUrl;
+});
+
+document.getElementById('openPageButton4').addEventListener('click', function() {
+    
+    var targetPageUrl = 'shortCourses.html'; // Replace with your target page URL
+
+    
+    window.location.href = targetPageUrl;
+});
+
